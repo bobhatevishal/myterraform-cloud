7bf19e305ca2dc463b4756e6918d905053cb8a17
Bobhate Vishal <bobhatev13@gmail.com>
2025-08-25 11:43:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,23 @@ provider "aws" {
   region = "ap-south-1"
  
 }
- 
+
+variable "access_key" {
+  description = "AWS access key"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "AWS secret key"
+  type        = string
+}
+
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
 # launch ec2 instance
 resource "aws_instance" "myec2" {
 ami = "ami-0f918f7e67a3323f0"
